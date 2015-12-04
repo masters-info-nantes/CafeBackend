bdb5e000a4b6c57249311f395cc158268ae8e6e1
Hector LABANCA <E15A968H@S046V7pc03.ensinfo.sciences.univ-nantes.prive>
2015-12-04 12:47:24 +0100

    fini expo rest

--- services-rest/src/main/java/fr/alma/mw1516/services/backend/BackendController.java
+++ services-rest/src/main/java/fr/alma/mw1516/services/backend/BackendController.java
@@ -3,7 +3,6 @@ package fr.alma.mw1516.services.backend;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,12 +20,12 @@ public class BackendController {
     @RequestMapping(value="bonjour", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<String> bonjour() {
-    	return new ResponseEntity<String>("",HttpStatus.OK);
+    	return new ResponseEntity<String>("Identification auprès du GI requise",HttpStatus.UNAUTHORIZED);
     }
     
     @RequestMapping(value="callback", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<String> callback(@RequestParam(value = "token", required = false) String token) {
+    public ResponseEntity<String> callback(@RequestParam(value = "token", required = true) String token) {
     	try {
 			back.callback(token);
 		} catch (Exception e) {
@@ -38,20 +37,40 @@ public class BackendController {
     @RequestMapping(value="solde", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<String> solde() {
-    	return new ResponseEntity<>(HttpStatus.OK);
+		try {
+			double solde = back.solde();
+			return new ResponseEntity<>("{solde:"+solde,HttpStatus.OK);
+		} catch (Exception e) {
+			System.out.println("Erreur solde : "+e.getMessage());
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 
     }
     @RequestMapping(value="credit", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<String> credit() {
+    public ResponseEntity<String> credit(@RequestParam(value = "somme", required = true) double somme) {
+    	try {
+			back.credit(somme);
+		} catch (Exception e) {
+			System.out.println("Erreur credit : "+e.getMessage());
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
     	return new ResponseEntity<>(HttpStatus.OK);
-
     }
+    
     @RequestMapping(value="acheter", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<String> acheter() {
-    	return new ResponseEntity<>(HttpStatus.OK);
-    	
+    public ResponseEntity<String> acheter(@RequestParam(value = "nomProduit", required = true) String nomProduit) {
+    	try {
+			if (back.acheter(nomProduit)) {				
+				return new ResponseEntity<>(HttpStatus.OK);    	
+			} else {
+				return new ResponseEntity<>("Erreur : solde insuffisant", HttpStatus.BAD_REQUEST);
+			}
+		} catch (Exception e) {
+			System.out.println("Erreur acheter : "+e.getMessage());
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
     }
 
 
